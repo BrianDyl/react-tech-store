d9518c7ed364c8a29037375fa156275497320df4
Brian <brianadyl@gmail.com>
2020-08-27 15:53:15 -0400

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import Home from './pages/HomePage';
+import About from './pages/AboutPage';
+import Cart from './pages/CartPage';
+import Contact from './pages/ContactPage';
+import Default from './pages/DefaultPage';
+import Products from './pages/ProductsPage';
+import SingleProduct from './pages/SingleProductPage';
+
+import {Route, Switch} from 'react-router-dom';
+
+
 class App extends Component {
   render(){
-    return <h1>Hello from Tech Store</h1>
+    return <>
+    {/* navbar, sidebar, cart, footer */}
+    <Switch>
+      <Route path="/" exact component={Home}/>
+      <Route path="/about" component={About}/>
+      <Route path="/contact" component={Contact}/>
+      <Route path="/products" exact component={Products}/>
+      <Route path="/products/:id" component={SingleProduct}/>
+      <Route path="/cart"  component={Cart}/>
+      <Route component={Default}/>
+    </Switch>
+    </>
   }
 }
 
